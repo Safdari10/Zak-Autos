c0a04204e2c31b6fba64f545cf08d8572433200b
Zaker Safdari <zakersafdari@gmail.com>
2024-11-06 15:55:23 +1300

    reduced gallery section items sizes. to give better responsiveness.

--- src/components/VehicleGallery.tsx
+++ src/components/VehicleGallery.tsx
@@ -4,32 +4,32 @@ import benz from "../assets/benz.png";
 
 const VehicleGallery = () => {
   return (
-    <div className="flex justify-center items-center gap-10 md:gap-48 flex-wrap bg-blue-800 p-4 md:p-12">
-      <div className="flex flex-col justify-center items-center w-96 md:w-[650px] p-2 md:p-4 bg-blue-600 rounded-lg shadow-md cursor-pointer transition-transform transform hover:scale-105">
+    <div className="flex justify-center items-center gap-10 md:gap-44 flex-wrap bg-blue-800 p-4 md:p-12">
+      <div className="flex flex-col justify-center items-center w-96 md:w-[550px] p-2 md:p-4 bg-blue-600 rounded-lg shadow-md cursor-pointer transition-transform transform hover:scale-105">
         <img
           src={bmw}
           alt="BMW"
-          className="w-full h-56 md:w-[620px] md:h-[420px] object-cover rounded"
+          className="w-full h-56 md:w-[500px] md:h-[380px] object-cover rounded"
         />
         <p className="mt-2 text-lg font-semibold text-white">BMW</p>
         <p className="text-gray-200 text-xl">Browse for latest arrivals</p>
       </div>
 
-      <div className="flex flex-col justify-center items-center w-96 md:w-[650px] p-2 md:p-4 bg-blue-600 rounded-lg shadow-md cursor-pointer transition-transform transform hover:scale-105">
+      <div className="flex flex-col justify-center items-center w-96 md:w-[550px] p-2 md:p-4 bg-blue-600 rounded-lg shadow-md cursor-pointer transition-transform transform hover:scale-105">
         <img
           src={benz}
           alt="Benz"
-          className="w-full h-56 md:w-[620px] md:h-[420px] object-cover rounded"
+          className="w-full h-56 md:w-[480px] md:h-[380px] object-cover rounded"
         />
         <p className="mt-2 text-lg font-semibold text-white">Benz</p>
         <p className="text-gray-200 text-xl">Browse for latest arrivals</p>
       </div>
 
-      <div className="flex flex-col justify-center items-center w-96 md:w-[650px] p-2 md:p-4 bg-blue-600 rounded-lg shadow-md cursor-pointer transition-transform transform hover:scale-105">
+      <div className="flex flex-col justify-center items-center w-96 md:w-[550px] p-2 md:p-2 bg-blue-600 rounded-lg shadow-md cursor-pointer transition-transform transform hover:scale-105">
         <img
           src={audi}
           alt="Audi"
-          className="w-full h-56 md:w-[620px] md:h-[420px] object-cover rounded"
+          className="w-full h-56 md:w-[520px] md:h-[380px] object-cover rounded"
         />
         <p className="mt-2 text-lg font-semibold text-white">Audi</p>
         <p className="text-gray-200 text-xl">Browse for latest arrivals</p>
