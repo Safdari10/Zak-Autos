ee6412f908dd479b2be1b6880416e27233f048d8
Zaker Safdari <zakersafdari@gmail.com>
2024-11-07 16:16:16 +1300

    organised the stylings according to screen size for hero section to avoid overriding moving from smaller to bigger. also adjusted text size and search box and button sizes for taller screens.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,23 +10,23 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black bg-opacity-50 z-10" />
 
       {/* Overlay for Text */}
-      <div className="relative flex flex-col justify-center items-center w-full md:w-1/2 p-6 md:p-20 md:mt-20 md:ml-10 z-20 text-white">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 text-center">
+      <div className="relative flex flex-col justify-center items-center p-6 text-white z-20 w-full md:w-1/2 md:p-20 md:mt-20 md:ml-10">
+        <h1 className="text-2xl font-bold text-center mb-4 sm:text-3xl md:text-4xl taller:text-6xl">
           Welcome to <span className="text-white shadow-gray-100">ZAK AUTOS</span>
         </h1>
-        <p className="text-sm sm:text-md md:text-lg mb-4 text-center">
+        <p className="text-sm text-center mb-4 sm:text-md md:text-lg taller:text-4xl">
           Find your dream car with us today!
         </p>
       </div>
 
-      {/* Search Box with Unified Width on Mobile */}
-      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-20 w-full max-w-xs md:max-w-lg flex flex-col md:flex-row items-center justify-center space-y-2 md:space-y-0">
+      {/* Search Box */}
+      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-20 w-full max-w-xs flex flex-col items-center justify-center space-y-2 md:max-w-xl md:flex-row md:space-y-0 taller:max-w-[1200px] taller:p-10">
         <input
           type="text"
           placeholder="Search for cars..."
-          className="border border-gray-300 rounded-md py-4 px-4 w-full md:w-[70%] text-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="border border-gray-300 rounded-md w-full py-4 px-4 text-lg focus:outline-none focus:ring-2 focus:ring-blue-500 md:w-[70%] taller:h-28 taller:text-4xl taller:w-[1000px]"
         />
-        <button className="bg-blue-500 text-white py-4 px-6 text-lg rounded-md w-full md:w-32 md:px-8 hover:bg-blue-600">
+        <button className="bg-blue-500 text-white rounded-md w-full py-4 px-6 text-lg hover:bg-blue-600 md:w-32 md:px-8 taller:text-4xl taller:w-[200px] taller:h-28">
           Search
         </button>
       </div>
